79a075257f6773b7e4a7973b1739736d0dcf3b51
zer0charisma <natalie.d.rees@gmail.com>
2023-12-28 12:40:18 -0600

    initial commit and changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// Q SOLUTION!!!!
 
+import React from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import UserProfile from "./UserProfile";
+
+function Home() {
+  return <p>Home</p>;
+}
+function NewUser() {
+  return <h1>Unable to create a new user</h1>;
+}
+function NotFound() {
+  return <h1>404 Not Found</h1>;
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Link to="/">Home</Link>
+        <br/>
+        <Link to="/user/new">New User</Link>
+      </div>
+      {Array(10)
+          .fill()
+          .map((ignoredValue, index) => index + 1)
+          .map((id) => (
+            <div key={id}>
+              <Link to={`/user/${id}`} data-testid={`user-${id}`}></Link>
+            </div>
+          ))}
+        <Switch>
+          <Route exact={true} path="/">
+            <Home />
+          </Route>
+        <Route path="/user/new"> 
+            <NewUser />
+          </Route>
+        <Route path="/user/:userId">
+            <UserProfile />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+      </Switch>
     </div>
   );
 }
-
 export default App;
